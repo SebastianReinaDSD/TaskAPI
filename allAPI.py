7afc71d097b7e008f8a9b991118cf0824cda4979
SebastianReinaDSD <85364859+SebastianReinaDSD@users.noreply.github.com>
2021-09-27 14:02:02 -0500

    Update allAPI.py

--- allAPI.py
+++ allAPI.py
@@ -5,8 +5,8 @@ import requests ,json
 
 
 
-token = 'secret_pIqDsfSIx4rWqucnCMY3PoDz6DxWYWkd8FpniQW34VD'
-databaseID = '3574f42993fa4efe90f5e4c7f7c2c112'
+token = 'yourtokensecret'
+databaseID = 'yourdatabaseID'
 tasks=None
 mylcd = I2C_LCD_driver.lcd()
 pageId=None
